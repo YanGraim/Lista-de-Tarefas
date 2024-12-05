59ea3bd580897642a801158c3f702884a6f6baee
YanGraim <yangraim@gmail.com>
2024-12-05 12:15:46 -0300

    lista tarefas

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ let buttonElement = document.querySelector("#app button");
 
 let tarefas = [];
 
+function renderizarTarefas() {
+    listElement.innerHTML = '';
+    tarefas.map((tarefa  ) => {
+        let liElement = document.createElement("li");
+        let tarefaText = document.createTextNode(tarefa);
+
+        liElement.appendChild(tarefaText);
+        listElement.appendChild(liElement);
+    })
+}
+
 function adicionarTarefas() {
     if(inputElement.value === '') {
         alert("Digite alguma tarefa!");
@@ -12,6 +23,7 @@ function adicionarTarefas() {
         let novaTarefa = inputElement.value;
         tarefas.push(novaTarefa);
         inputElement.value = '';
+        renderizarTarefas();
     }
 }
 
